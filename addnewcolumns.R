c4bddc468494202e4230c0f833a3e5147668bee9
mwong9790 <52176747+mwong9790@users.noreply.github.com>
2019-07-31 21:59:58 -0400

    Add files via upload

--- addnewcolumns.R
+++ addnewcolumns.R
@@ -4,8 +4,8 @@ install.packages("dplyr")
 library("dplyr")
 #import datasets
 pluto_bbl=read.csv("BBL_data_count_by_BBL.csv")
-race = read.csv("ACS_14_5YR_B02001_Race.csv")
-income = read.csv("ACS_14_5YR_B19013_median_income.csv")
+race = read.csv("ACS_14_5YR_B03002_Race.csv")
+income = read.csv("ACS_14_5YR_B19025_agg_income.csv")
 nycha = read.csv("COLP_2018.csv")
 #edit race to have Borough, CensusTract, and Census Block Group values
 race$CT2010pt1 <- as.integer(substr(race$Id2,6,9))
@@ -56,10 +56,10 @@ nycha1$nycha <- 'Y'
 #creating NYCHA indicator
 pluto_race_income$BBL <- as.character(pluto_race_income$BBL)
 pluto_all <- left_join(x=pluto_race_income,y=nycha1,by="BBL",all.x=TRUE)
-pluto_all_short <- subset(pluto_all,select=c(BBL,CT2010.y,CBlockGroup.x,YearBuilt.x,UnitsRes.x,SUnits_by_bbl.x,HViol_by_bbl.x,Total,White,Black,Native_American,Asian,Hawaiian_or_Pacific_Islander,Other_Race,Two._Races,Two._Races_not_Other,Three._Races_not_Other,Median_HH_Income,nycha))
+pluto_all_short <- subset(pluto_all,select=c(BBL,CT2010.y,CBlockGroup.x,YearBuilt.x,UnitsRes.x,SUnits_by_bbl.x,HViol_by_bbl.x,Total,White,Black,Native_American,Asian,Hawaiian_or_Pacific_Islander,Latino,Other_Race,Two._Races_NL,Two._Races_not_Other_NL,Three._Races_not_Other_NL,Two._Races_Latino,Two._Races_not_Other_Latino,Three._Races_not_Other_Latino,Agg_HH_Income,nycha))
 
 #renaming
-pluto_all_short <- pluto_all_short %>% rename(CT2010 = CT2010.y, CBlockGroup = CBlockGroup.x, YearBuilt=YearBuilt.x,UnitsRes=UnitsRes.x,SUnits=SUnits_by_bbl.x,HViol=HViol_by_bbl.x,TotalRace=Total,TwoRaces=Two._Races,TwoRacesNotOther=Two._Races_not_Other,ThreeRacesNotOther=Three._Races_not_Other)
+pluto_all_short <- pluto_all_short %>% rename(CT2010 = CT2010.y, CBlockGroup = CBlockGroup.x, YearBuilt=YearBuilt.x,UnitsRes=UnitsRes.x,SUnits=SUnits_by_bbl.x,HViol=HViol_by_bbl.x,TotalRace=Total,TwoRacesNL=Two._Races_NL,TwoRacesNotOtherNL=Two._Races_not_Other_NL,ThreeRacesNotOtherNL=Three._Races_not_Other_NL,TwoRacesL=Two._Races_Latino,TwoRacesNotOtherL=Two._Races_not_Other_Latino,ThreeRacesNotOtherL=Three._Races_not_Other_Latino)
 
 #to export data
 write.csv(pluto_all_short, file = "BBL_all.csv")
